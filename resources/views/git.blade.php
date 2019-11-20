f553482534d74ba0dcca84e92f787fcd153969c8
“ajuanenama” <ajuanenama@zubirimanteo.com>
2019-11-20 09:08:09 +0100

    subiendo los dos primeros ejercicios

--- resources/views/git.blade.php
+++ resources/views/git.blade.php
@@ -4,7 +4,15 @@
 
 @section('content')
 
-  Introduce el resultado del ejercicio de git (ejercicio 2).
+  <h3>¿Qué deberías realizar para fusionar esta rama con la rama master? Indica los comandos necesarios para ello.</h3>
+  <p>git merge branch-name  </p>
+  <hr>
+  <h3>Indica en que consiste el modelo vista-controlador. Describe brevemente cada uno de los componentes que lo forman. </h3>
+  <ul>
+    <li>Vista: La vista muestra informacion al usuario</li>
+    <li>Controlador: El controlador interactua con el usuario, por ejemplo le indica donde esta la view .</li>
+    <li>Modelo: El.</li>
+  </ul>
 
 
 @endsection
